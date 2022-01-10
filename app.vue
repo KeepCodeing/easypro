b6cd8abc2a45ca65fcb0bc04e792a19a58569f77
KeepCodeing <3350680655@qq.com>
2022-01-10 15:17:14 +0800

    build: 初始化项目

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <h3 class="text-red-500">Hello Nuxt</h3> -->
+    <NuxtPage></NuxtPage>
+    <!-- PageIndex -->
+  </div>
+</template>
+
+<script setup>
+import "@/assets/css/tailwind.css";
+// export default {};
+</script>
