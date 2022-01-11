59f47eabb9e87050ac3d1d710c61ab1477d128bc
KeepCodeing <3350680655@qq.com>
2022-01-11 11:11:06 +0800

    feat: 增加课程介绍页面

---
+++ pages/course/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtLayout name="main-layout">
+      <template #content> I a course </template>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  layout: false,
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+</style>
