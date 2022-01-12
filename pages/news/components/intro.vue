289f0bf0fd12d1b229bf827d30acf0bdeac882c8
KeepCodeing <3350680655@qq.com>
2022-01-12 13:26:37 +0800

    feat(news/intro): 新闻页面的公司简介完成

---
+++ pages/news/components/intro.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <card-list colStyle="md:col-span-4 col-span-12 mt-20" :data="data">
+      <template #content="scoped">
+        <div
+          class="h-full p-10 bg-white rounded-lg sm:p-5"
+          style="background-color: rgb(244, 242, 248)"
+        >
+          <div class="text-center md:mb-5 icon" style="margin-top: -35%">
+            <img
+              class="inline w-2/5 md:w-1/2 sm:w-1/3 aspect-square"
+              :src="scoped.item.img"
+            />
+          </div>
+          <div
+            class="text-xl font-black text-center text-black  lg:text-4xl md:text-3xl sm:text-2xl"
+          >
+            {{ scoped.item.title }}
+          </div>
+          <div
+            class="mt-10 font-serif text-base font-bold text-gray-400  lg:text-2xl md:text-xl sm:text-lg"
+          >
+            {{ scoped.item.info }}
+          </div>
+        </div>
+      </template>
+    </card-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+@media (max-width: 768px) {
+  .icon {
+    margin-top: -20% !important;
+  }
+}
+@media (max-width: 640px) {
+  .icon {
+    margin-top: -30% !important;
+  }
+}
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <NuxtLayout name="main-layout">
+      <template #hero>
+        <div class="relative flex items-center justify-center py-24">
+          <div class="text-center text-white">
+            <h1 class="text-4xl font-black">易编程在中国</h1>
+            <h4 class="px-5 mt-5 text-xl font-medium">
+              我们希望将编程教育带入中小学当中，让更多的孩子有机会接触、了解到编程。
+            </h4>
+          </div>
+          <div
+            style="background-image: url('img/关于-中图2.png')"
+            class="absolute w-full h-full bg-center bg-cover -z-10 hero"
+          ></div>
+        </div>
+      </template>
+      <template #content>
+        <div class="px-10 my-10 lg:my-20 lg:px-20 xl:px-40">
+          <Intro :data="data.intro"></Intro>
+        </div>
+      </template>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import jdata from "@/data/news.json";
+import Intro from "./components/intro.vue";
+
+export default defineComponent({
+  components: { Intro },
+  setup() {
+    const data = reactive(jdata);
+    return {
+      data,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.hero::after {
+  background-image: linear-gradient(
+    -180deg,
+    rgba(0, 0, 0, 0.35) 0%,
+    rgba(0, 0, 0, 0.7) 81%
+  );
+  content: "";
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+}
+</style>
